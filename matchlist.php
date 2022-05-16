c111e3955f2c727317528f4797c1e9eb5158ac9c
Zeynepnur Cavcar <zeynepnur.cavcar@ug.bilkent.edu.tr>
2022-05-16 10:47:35 +0300

    Match list completed but errors with frontend

--- matchlist.php
+++ matchlist.php
@@ -1,3 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-?>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+
+    <title>Host Cloud Template - Services</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-host-cloud.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+<!--
+
+Host Cloud Template
+
+https://templatemo.com/tm-541-host-cloud
+
+-->
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><h2>BETMAN</h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">My Wallet</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="services.html">Matches</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <!-- Heading Starts Here -->
+    <div class="page-heading header-text">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1>Our Services</h1>
+            <p><a href="index.html">Home</a> / <span>Our Services</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Heading Ends Here -->
+
+
+    <!-- Services Starts Here -->
+    <div class="services-section services-page">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Past and Upcoming Matches</h2>
+            </div>
+          </div>
+          <?php
+            include "config.php";
+            $con = $connection;
+
+            $query = "SELECT match_id, match_type, match_date FROM Game";
+            $complist = mysqli_query($connection, $query);
+
+            // find the teams of the match
+            while($tuple = mysqli_fetch_array($complist)) {
+                $tmp = $tuple['match_id'];
+                $query2 = "SELECT Host.team_name AS host, Guest.team_name AS guest 
+                            FROM TeamsPlaying, Team AS Host, Team AS Guest
+                            WHERE match_id = '" .$tmp ."' AND host_id = Host.team_id AND guest_id = Guest.team_id";
+                $teams = mysqli_query($connection, $query2);
+                $teamtuple = mysqli_fetch_array($teams);
+
+                echo("<div class='col-md-4 col-sm-6 col-xs-12'>
+                        <div class='match'>");
+                echo("<h5>".$teamtuple['host']." - ".$teamtuple['guest']."</h5>");
+                echo("<h5>".$tuple['match_type']."</h5>");
+                echo("<p>".$tuple['match_date']."</p");
+                echo("</div></div>");
+            }
+          ?>
+        </div>
+      </div>
+    </div>
+    <!-- Services Ends Here --> 
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+  </body>
+</html>
